4f32fc2552c606113fa2a8d0cc0f2bcdcc9b8ba9
Utka1239 <samsitov@list.ru>
2023-01-13 15:33:22 +0500

    add Example6_Base2

--- Example6_Base/Program.cs
+++ Example6_Base/Program.cs
@@ -7,8 +7,8 @@ int max = a;
 if (a > max ) max = a;
 if (b > max ) max = b;
 if (c > max ) max = c;
-if (e > max ) max = d;
-if (d > max ) max = e;
+if (d > max ) max = d;
+if (e > max ) max = e;
 
-Console.Write("max =");
+Console.Write("max = ");
 Console.WriteLine(max);
