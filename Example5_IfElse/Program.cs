d0c0f613caa1744e8780be64955c5d4fc02aeec8
Utka1239 <samsitov@list.ru>
2023-01-13 15:12:09 +0500

    add Example5_IfElse

---
+++ Example5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же САША!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
